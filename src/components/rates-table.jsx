bbee681e458668167640f824241fa551a75877c2
strmdctem <strmdctem@gmail.com>
2024-05-20 17:30:29 +0000

    second commit

---
+++ src/components/rates-table.jsx
@@ -0,0 +1,79 @@
+import { useMemo } from 'react';
+import {
+  MaterialReactTable,
+  useMaterialReactTable
+} from 'material-react-table';
+import rates from '../data/rates.json'; // adjust the path as needed
+
+
+//nested data is ok, see accessorKeys in ColumnDef below
+const data = rates.map((item) => {
+  const rates = item.rates.main.reduce((acc, rate) => {
+    const key = `${rate.start}-${rate.end}`;
+    acc[key] = rate.general;
+    return acc;
+  }, {});
+
+  return {
+    name: item.name,
+    key: item.key,
+    type: item.type,
+    ...rates,
+  };
+});
+
+const RatesTable = () => {
+  //should be memoized or stable
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'name',
+        header: 'Name',
+        size: 5,
+      },
+      {
+        accessorKey: '7-14', //access nested data with dot notation
+        header: '7-14D',
+        size: 0,
+        enableColumnPinning: false,
+      },
+      {
+        accessorKey: '15-29',
+        header: '15-29D',
+        size: 0,
+        enableColumnPinning: false,
+      },
+      {
+        accessorKey: '30-45',
+        header: '30-45D',
+        size: 5,
+        enableColumnPinning: false,
+      },
+      {
+        accessorKey: '46-60',
+        header: '46-60D',
+        size: 0,
+        enableColumnPinning: false,
+      }
+    ],
+    [],
+  );
+
+  const table = useMaterialReactTable({
+    columns,
+    data,
+    enablePagination: false,
+    enableBottomToolbar: false,
+    enableColumnFilter: false,
+    enableColumnActions: false,
+    enableColumnPinning: true,
+    initialState: {
+      showGlobalFilter: true,
+      columnPinning: { left: ['name'], right: [] },
+    },
+  });
+
+  return <MaterialReactTable table={table} />;
+};
+
+export default RatesTable;
--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark'
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
