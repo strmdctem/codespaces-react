fb5a7c0b0620861ab3ebe35e50e15d7c418e7442
strmdctem <strmdctem@gmail.com>
2024-06-15 10:33:41 +0530

    updated

--- src/components/fd-scheme/scheme.jsx
+++ src/components/fd-scheme/scheme.jsx
@@ -1,33 +1,28 @@
-import * as React from 'react';
 import { useState } from 'react';
-import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
+import { FormControl, ToggleButton, ToggleButtonGroup } from '@mui/material';
 
 export default function FDSchemeSelector({ onChange }) {
-    const [selectedChip, setSelectedChip] = useState("Regular");
 
-    const handleClick = (label) => {
-        setSelectedChip(label);
-        onChange(label);
+    const [value, setValue] = useState("Regular");
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue); // Set the state variable
+        onChange(newValue);
     };
 
-    const getVariant = (chip) => selectedChip === chip ? "default" : "outlined";
-
-    const regularVariant = getVariant("Regular");
-    const specialVariant = getVariant("Special");
-
     return (
-        <Stack direction="row" spacing={2} sx={{ m: 1 }}>
-            <Chip sx={{ p: 2 }} color="primary"
-                label="Regular"
-                onClick={() => handleClick("Regular")}
-                variant={regularVariant}
-            />
-            <Chip sx={{ p: 2 }} color="primary"
-                label="Special"
-                onClick={() => handleClick("Special")}
-                variant={specialVariant}
-            />
+        <Stack direction="row" sx={{ m: 1 }}>
+            <FormControl variant="filled">
+                <ToggleButtonGroup exclusive size="small" color="primary" value={value} onChange={handleChange}>
+                    <ToggleButton value="Regular" sx={{ px: 2 }}>
+                        Regular
+                    </ToggleButton>
+                    <ToggleButton value="Special" sx={{ px: 2 }}>
+                        Special
+                    </ToggleButton>
+                </ToggleButtonGroup>
+            </FormControl>
         </Stack>
     );
 }
