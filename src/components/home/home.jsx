929f34daa79691aae9c567c8615910a696fddffa
strmdctem <strmdctem@gmail.com>
2024-08-15 18:52:35 +0530

    updated

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,8 +1,9 @@
-import { Paper, Stack, Typography } from '@mui/material';
+import CalculateOutlinedIcon from '@mui/icons-material/CalculateOutlined';
+import TableChartOutlinedIcon from '@mui/icons-material/TableChartOutlined';
+import { Button, Paper, Stack, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
-
 export default function Home({ isDarkMode }) {
   const videoRef1 = useRef(null);
   const videoRef2 = useRef(null);
@@ -55,11 +56,7 @@ export default function Home({ isDarkMode }) {
   return (
     <Box sx={{ p: 3 }}>
       <Paper elevation={0}>
-        <Typography
-          variant="subtitle1"
-          className="home-intro"
-          sx={{ mt: 0, fontStyle: 'italic' }}
-        >
+        <Typography variant="subtitle1" className="home-intro" sx={{ mt: 0 }}>
           Assisting Indians to <label>Compare, Calculate and Optimize</label>
           &nbsp;their Savings with comprehensive calculator, screener, insights
           and the latest interest rates from 30+ Banks and NBFCs across the
@@ -67,10 +64,87 @@ export default function Home({ isDarkMode }) {
         </Typography>
       </Paper>
       <br />
-      <Stack gap={2}>
-        <Paper elevation={1}>FD Screener - All Rates</Paper>
-        <Paper elevation={1}>FD - Calculate & Compare</Paper>
+      <Stack gap={3}>
+        <Paper elevation={2}>
+          <Link to={`/fixed-deposit`}>
+            <Button
+              variant="outlined"
+              fullWidth
+              sx={{ textTransform: 'initial', borderColor: '#e0e0e0' }}
+            >
+              <TableChartOutlinedIcon fontSize="small" sx={{ mr: 1 }} />
+              Fixed Deposit Screener - All Rates
+            </Button>
+          </Link>
+        </Paper>
+        <Paper elevation={2}>
+          <Link to={`/fixed-deposit/calculator`}>
+            <Button
+              variant="outlined"
+              color="primary"
+              fullWidth
+              sx={{ textTransform: 'initial', borderColor: '#e0e0e0' }}
+            >
+              <CalculateOutlinedIcon fontSize="small" sx={{ mr: 1 }} />
+              Fixed Deposit - Calculate & Compare
+            </Button>
+          </Link>
+        </Paper>
+        <Paper elevation={2}>
+          <Link to={`/fixed-deposit/calculator`}>
+            <Button
+              variant="outlined"
+              color="primary"
+              fullWidth
+              sx={{ textTransform: 'initial', borderColor: '#e0e0e0' }}
+            >
+              Dummy option - any suggestions?
+            </Button>
+          </Link>
+        </Paper>
       </Stack>
+      <br />
+      <Stack>
+        <Typography
+          variant="h6"
+          sx={{
+            fontSize: 16,
+            fontWeight: 'bold'
+          }}
+        >
+          <Link to={`/fixed-deposit`} className="menu-link">
+            Fixed Deposit Screener
+          </Link>
+        </Typography>
+        <Typography variant="body2">
+          Compare the latest fixed deposit rates from leading banks and NBFCs to
+          find the best options for your savings.
+        </Typography>
+        <img
+          src="fd-screener-2.jpg"
+          style={{ padding: '20px', paddingRight: '30px' }}
+        />
+        <Typography
+          variant="h6"
+          sx={{
+            fontSize: 16,
+            fontWeight: 'bold'
+          }}
+        >
+          <Link to={`/fixed-deposit/calculator`} className="menu-link">
+            Fixed Deposit Calculator
+          </Link>
+        </Typography>
+        <Typography variant="body2">
+          Utilize our precise calculators to discover the best returns on your
+          investments.
+        </Typography>
+        <img
+          src="fd-calculator.jpg"
+          style={{ padding: '20px', paddingRight: '30px' }}
+        />
+      </Stack>
+
       <div className="carousel">
         <div className="carousel-inner">
           <div className="carousel-item">
@@ -85,80 +159,6 @@ export default function Home({ isDarkMode }) {
         </div>
       </div>
       <img src="/Stats.svg" className="stats-img" alt="Insight 1" />
-      <Typography
-        className="home-heading"
-        variant="h6"
-        sx={{
-          mt: 3,
-          fontSize: 18,
-          fontWeight: 'bold'
-        }}
-      >
-        Fixed Deposit
-      </Typography>
-      <Link className="home-link" to="/fixed-deposit">
-        Rates Table
-      </Link>
-      <Typography
-        variant="subtitle1"
-        sx={{
-          mb: 1
-        }}
-      >
-        Compare the latest fixed deposit rates from leading banks and NBFCs to
-        find the best options for your savings.Lorem Ipsum is simply dummy text
-        of the printing and typesetting industry.
-      </Typography>
-      <Stack className="video-cnt">
-        <video
-          ref={videoRef1}
-          key={isDarkMode ? 'dark' : 'light'}
-          width="100%"
-          height="auto"
-          muted
-          loop
-          playsInline
-        >
-          <source
-            src={
-              isDarkMode
-                ? 'video-screener-dark.mp4'
-                : 'video-screener-light.mp4'
-            }
-            type="video/mp4"
-          />
-        </video>
-      </Stack>
-      <Link className="home-link" to="/fixed-deposit/calculator">
-        Calculator
-      </Link>
-      <Typography
-        variant="subtitle1"
-        sx={{
-          mt: 0,
-          mb: 2
-        }}
-      >
-        Utilize our precise calculators to discover the best returns on your
-        investments.Lorem Ipsum has been the industry&rsquo;s standard dummy
-        text ever since the 1500s.
-      </Typography>
-      <div className="video-cnt-1">
-        <video
-          ref={videoRef2}
-          key={isDarkMode ? 'dark' : 'light'}
-          width="100%"
-          height="auto"
-          muted
-          loop
-          playsInline
-        >
-          <source
-            src={isDarkMode ? 'video-calc-dark.mp4' : 'video-calc-light.mp4'}
-            type="video/mp4"
-          />
-        </video>
-      </div>
     </Box>
   );
 }
